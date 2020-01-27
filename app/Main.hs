52747204aca994b7542fdd32fb6d1932772b0349
増田 翔仁 <masudashojin@shojinmasudanoMacBook-Pro-3.local>
2020-01-27 20:26:24 +0900

    hoge

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,44 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE BangPatterns #-}
-module Main where
-import Control.Monad
-import Lib
--- import qualified Data.Vector as V
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
-data Hoge = Hoge Int Int Int Int Int Int Int Int Int Int Int Int deriving(Show)
-data Huga = Huga !Int !Int !Int !Int !Int !Int !Int !Int !Int !Int !Int !Int  deriving(Show)
-#define Type Hoge
+module Main where
+
+import Control.Monad
+import GHC.Generics
+import Data.Proxy
+import GHC.TypeLits
+
+data Permission' a = Authorized a | NoPermission
+    deriving ( Show )
+
+data Report = Report { cprice :: Permission' (Maybe Double) }
+    deriving ( Generic, Show )
+
+class HasPermField (fname :: Symbol) a rt where
+    getField :: Proxy fname -> a -> Permission' (Maybe rt)
+
+instance HasPermField "cprice" Report Double where
+    getField _ = cprice
+
+data PermField (fname :: Symbol) a rt
+
+data PermFields :: [ Symbol ] -> * -> * where
+    End :: PermFields fields repo -> PermFields (y ': fields) repo
+    Cont :: (HasPermField fname a rt)
+        => PermField fname a rt -> PermFields fields repo
+
+
+
+encode :: PermFields xs t -> Report -> String
+encode _ r = undefined
+
 main :: IO ()
 main = do
-  someFunc
-  --let !xs = replicate (1000000*1) (Type 100 200 300 100 200 300 100 200 300 100 200 300)
-  --seq xs $ pure ()
-  ---- forM_ xs $ \x -> do
-  ----   seq x $ pure ()
-  --print $ foldr  (\(Type a b c d e f g h i j k l) x -> x + a+b+c+d+e+f+g+h+i+j+k+l) (0 :: Int) xs
+  print $ encode undefined Report {cprice = Authorized (Just 1)}
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -7,7 +8,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedLabels #-}
 module Lib
-    ( someFunc
+    ( 
     ) where
 import Data.List
 import GHC.Generics
@@ -15,25 +16,32 @@ import Data.Proxy
 import Data.Data
 import Data.Typeable
 import Data.Extensible
-import Control.Lens
 
-type Hoge = Record '["id" >: Int, "status" >: Maybe String ]
-type Huga = Record '["id" >: Int, "status" >: String ]
-
-type family ToMaybeRec a
-
-type instance ToMaybeRec Huga = Hoge
-
-f :: forall a. a -> ToMaybeRec a
-f x = let stat = Just $ x ^. #status
-       in shrink $ #status @= stat <: x
-
-
-r1 :: Hoge
-r1 = #id @= 0 <: #status @= Nothing <: emptyRecord 
-
-
-someFunc :: IO ()
-someFunc = do 
-  print r1
-  putStrLn "someFunc"
+--type HId = '["id" >: Int]
+--type Hoge = Record  (HId >: '["status" >: Maybe String ] )
+--type Huga = Record '["id" >: Int, "status" >: String ]
+--
+--type family ToMaybeRec a
+--
+--type instance ToMaybeRec Huga = Hoge
+--
+--collectiveOf :: (Associate "name" String s, Associate "collective" String s)
+--  => Record s -> String
+--collectiveOf x = concat $ [x ^. #name, x ^. #collective]
+--
+--f :: forall s t. (Lookup s "status" String , Lookup t "status'" (Maybe String)) => Record s -> Record t
+--f x = let stat = Just $ x ^. #status
+--       in #status' @= Nothing <: emptyRecord 
+--
+---- id, statusを持つ任意のものを返せないといけなくなるのでこういうことは書けない
+---- mkHoge :: forall a. (Lookup a "id" Int, Lookup a "status" (Maybe String)) => Record a
+---- mkHoge = #id @= 0 <: #status @= Nothing <: emptyRecord 
+--
+--r1 :: Hoge
+--r1 = #id @= 0 <: #status @= Nothing <: emptyRecord 
+--
+--
+--someFunc :: IO ()
+--someFunc = do 
+--  print r1
+--  putStrLn "someFunc"
